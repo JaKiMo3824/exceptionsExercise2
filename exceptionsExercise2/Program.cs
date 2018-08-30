8fcca5212a9a4c506446d6358f4571c2aac7e50c
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-29 19:13:11 -0700

    exceptionsExercise2
    exercises 1-5

--- exceptionsExercise2/Program.cs
+++ exceptionsExercise2/Program.cs
@@ -10,6 +10,37 @@ namespace exceptionsExercise2
     {
         static void Main(string[] args)
         {
+            bool validAnswer = false;
+            int userAge = 0;
+            try
+            {
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Hello! Please enter your age in years.");
+                    validAnswer = int.TryParse(Console.ReadLine(), out userAge);
+                    if (!validAnswer) Console.WriteLine("Please enter whole number digits only, no decimal numbers.");
+                    
+                }
+                if (userAge <= 0)
+                {
+                    throw new Exception();
+                }
+                int birthYear = 2018 - userAge;
+
+                Console.WriteLine("You were born in " + birthYear + ".");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter positive whole number digits only, no decimal numbers, zero, or negative numbers.");
+                Console.ReadLine();
+                return;
+            }
+            
+
+
+
+
         }
     }
 }
